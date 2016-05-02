2469375a0a22c7353853f9e8a56a739a840ecf87
Brian DOstilio <briand787b@gmail.com>
2016-05-02 18:51:49 -0400

    added atom feed and modified the order to reset cart upon successful purchase

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,16 +7,25 @@ class Order < ActiveRecord::Base
 	validates :pay_type, inclusion: PAYMENT_TYPES
 
 	def add_line_items_from_cart(cart)
+		#The code below did not work.  I assumed that there were invisible variables in the model that I could
+		#change by just re-assigning them new values, like I did below
+		@address = "I CHANGED THIS IN THE MODEL,  ITS LONG AND CAPPED SO I WOULD SEE IT EASILY"
 		cart.line_items.each do |item|
 			item.cart_id = nil
 			line_items << item
 		end
+		#The line below proves that line(15) appends a new value to the line_items collection for the order object
+		#line_items << LineItem.new(product_id: 3)
 		puts "**************************************"
 		puts "HERE BE THY LINE ITEMS"
 		line_items.each do |item|
-			puts item.product.title 
+			puts "Title: #{item.product.title}"
+			puts "Order: #{item.order}" 
 		end
 		puts "**************************************"
+		#Both of the below statements produce the same output
+		puts self.name
+		puts name
 		puts "**************************************"
 	end
 end
